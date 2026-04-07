07b9ae2bd0c82e4c9bfcb00c8bcc65eca9efcf74
Mohan-I <mohanshyadav@gmail.com>
2026-04-07 23:54:29 +0530

    changes-aos added

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -10,3 +10,14 @@ document.querySelectorAll('.navlinks a'). forEach(link => {
     navLinks.classList.remove('active');
     menuToggle.classList.remove('is-active');
 });
+
+
+window.addEventListener("load", () => {
+    const loader = document.querySelector(".loader-wrapper");
+
+    // Add a slight delay (e.g., 500ms) so the user actually sees the 
+    // cool animation if the site loads too fast.
+    setTimeout(() => {
+        loader.classList.add("loader-hidden");
+    }, 500);
+});
